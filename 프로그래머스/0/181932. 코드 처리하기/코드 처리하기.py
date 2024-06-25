d378ee53c30f9f53044b3fc6c917561a06977dfc
Ragna-X <53248076+Ragna-X@users.noreply.github.com>
2024-06-25 14:41:15 +0900

    [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,9 +1,9 @@
 def solution(code):
     mode = 0
-    answer = ""
+    answer = []
     for idx, char in enumerate(code):
         if char == "1":
             mode = 1 - mode
         elif (mode == 0 and idx % 2 == 0) or (mode == 1 and idx % 2 != 0):
-            answer += char
-    return answer if answer else "EMPTY"
+            answer.append(char)
+    return "".join(answer) if answer else "EMPTY"
