30a6300ea4e741642c7cf8bd9d5c842382219eb0
Ragna-X <53248076+Ragna-X@users.noreply.github.com>
2024-07-01 14:24:06 +0900

    [level 0] Title: 수 조작하기 2, Time: 13.05 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,9 +1,3 @@
 def solution(numLog):
-    move_map = {1: 'w', -1: 's', 10: 'd', -10: 'a'}
-    current = numLog[0]
-    strlog = '' 
-    for num in numLog[1:]:
-        move = num - current
-        strlog += move_map[move]
-        current = num
-    return strlog
+    answer = ''
+    return answer
