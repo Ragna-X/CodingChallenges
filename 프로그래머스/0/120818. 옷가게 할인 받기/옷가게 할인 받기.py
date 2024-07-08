341c65ce9d9248f2cc092799771bec3823f32f27
Ragna-X <53248076+Ragna-X@users.noreply.github.com>
2024-07-08 13:04:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,8 +1,9 @@
 def solution(price):
+    discount_rate = 1
     if 100000 <= price < 300000 :
-        return int(price * 0.95)
+        discount_rate = 0.95
     elif 300000 <= price < 500000:
-        return int(price * 0.9)
+        discount_rate = 0.9
     elif 500000 <= price:
-        return int(price * 0.8)
-    return price
+        discount_rate = 0.8
+    return int(price * discount_rate)
