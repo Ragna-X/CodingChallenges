cb0b0f0f2bf340ecd0958238d2b2b9ff7d584fbd
Ragna-X <53248076+Ragna-X@users.noreply.github.com>
2024-07-29 18:37:28 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,13 +1,3 @@
 def solution(balls, share):
-    
-    def factorial(n):
-        result = 1
-        for i in range(1, n + 1):
-            result *= i
-        return result
-    
-    total_factorial = factorial(balls)
-    selected_factorial = factorial(share)
-    remaining_factorial = factorial(balls - share)
-    
-    return total_factorial // (selected_factorial * remaining_factorial)
+    answer = 0
+    return answer
