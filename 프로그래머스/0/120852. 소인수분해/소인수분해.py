3a9aac7de1db006dfab9372fee5ffdc8ed90aca2
Ragna-X <53248076+Ragna-X@users.noreply.github.com>
2024-07-29 13:24:26 +0900

    [level 0] Title: 소인수분해, Time: 1.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,12 +1,3 @@
 def solution(n):
-    prime_factors = []
-    divisor = 2
-    
-    while n > 1:
-        if n % divisor == 0:
-            if divisor not in prime_factors:
-                prime_factors.append(divisor)
-            n //= divisor
-        else:
-            divisor += 1
-    return prime_factors
+    answer = []
+    return answer
