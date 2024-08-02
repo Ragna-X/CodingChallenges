34b81ae9e8043fb54041505da36c1302960ac7c8
Ragna-X <53248076+Ragna-X@users.noreply.github.com>
2024-08-02 19:33:31 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,10 +1,11 @@
 def solution(common):
     x1 = common[0]
     x2 = common[1]
+    x3 = common[2]
     c = x2 - x1
-    if x2 + c == common[2]:
+    if x2 + c == x3:
         return common[-1] + c
     
     d = x2 // x1
-    if x2 * d == common[2]:
+    if x2 * d == x3:
         return common[-1] * d
