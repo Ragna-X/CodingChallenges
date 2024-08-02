1b8603d7a1e9fbd2c1222b015fb6d19506a20b53
Ragna-X <53248076+Ragna-X@users.noreply.github.com>
2024-08-02 18:20:25 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,7 +1,12 @@
 def solution(A, B):
     if A == B:
         return 0
-    for i in range(1, len(A)):
-        if A[-i:] + A[:-i] == B:
+    
+    length = len(A)
+    
+    for i in range(1, length):
+        A = A[-1] + A[:-1]
+        if A == B:
             return i
+        
     return -1
