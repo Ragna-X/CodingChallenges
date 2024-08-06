47c36383c484c50a31561035778a5892c02069c5
Ragna-X <53248076+Ragna-X@users.noreply.github.com>
2024-08-06 23:19:14 +0900

    [level 0] Title: 옹알이 (1), Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,14 +1,12 @@
 def solution(babbling):
     count = 0
     valid_sounds = ["aya", "ye", "woo", "ma"]
-    result = []
     for babble in babbling:
         temp_babble = babble
         for sound in valid_sounds:
             temp_babble = temp_babble.replace(sound, "-", 1)
             
         if not temp_babble.replace("-", ""):
-            result.append(babble)
             count += 1
 
     return count
